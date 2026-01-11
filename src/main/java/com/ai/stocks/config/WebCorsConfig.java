f111020649af9b289c8fadb0101e2f78cc090702
Ata ul nasar <ataulnasar@gmail.com>
2026-01-11 18:08:53 +0100

    Add some config

--- src/main/java/com/ai/stocks/config/WebCorsConfig.java
+++ src/main/java/com/ai/stocks/config/WebCorsConfig.java
@@ -14,7 +14,8 @@ public class WebCorsConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173")
+        "http://localhost:5173",
+                "https://ai-portfolio-analysis-ui.onrender.com"
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*");
       }
